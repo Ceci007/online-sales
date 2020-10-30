a0a90651e12b43cedb2437388248d2a9a1edbcc9
solbencas <cecibenitezca@gmail.com>
2020-10-30 19:46:10 -0300

    Products routes, actions, model and controler before delete action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,37 @@
 class ProductsController < ApplicationController
   def index 
+    @products = Product.all
+  end
+
+  def show 
+    @product = Product.find(params[:id])
+  end 
+
+  def new 
+    @product = Product.new
+  end
+
+  def update 
+    @product = Product.find(params[:id]) 
+    @product.update(product_params)
+
+    redirect_to root_path
+  end
+
+  def create 
+    @product = Product.new(product_params)
+    @product.save
+
+    redirect_to root_path
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  private 
+
+  def product_params 
+    params.require(:product).permit(:name, :price, :description, :image_url)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   root to: "products#index"
+
+  get '/products/new', to: "products#new", as: "new_product"
+  get '/products', to: "products#index"
+  post '/products', to: "products#create"
+
+  get '/products/:id', to: "products#show", as: "product"
+  get '/products/:id/edit', to: "products#edit", as: "edit_product"
+  patch '/products/:id', to: "products#update"
 end
